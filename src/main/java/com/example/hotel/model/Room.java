9b126afa22150a158fa2a1abffaedaf29dc2a2cf
tejasghodake <tejasghodake24@gmail.com>
2025-07-29 20:53:21 +0530

    Created Room.java

---
+++ src/main/java/com/example/hotel/model/Room.java
@@ -0,0 +1,67 @@
+package com.example.hotel.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomNumber;
+
+    private String type;
+
+    private Double price;
+
+    private boolean available;
+
+    // Constructors
+    public Room() {}
+
+    public Room(String roomNumber, String type, Double price, boolean available) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.price = price;
+        this.available = available;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
