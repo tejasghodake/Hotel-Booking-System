709d52e872d920a9129ebdecc1150ec1ea01b2ab
tejasghodake <tejasghodake24@gmail.com>
2025-07-30 10:22:56 +0530

    Delete src/main/java/com/example/invoicejet/repository/InvoiceRepository.java

--- src/main/java/com/example/invoicejet/repository/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.invoicejet.repository;
-
-import com.example.invoicejet.model.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
